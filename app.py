d5e39ed2293a97c5ed2be2e72a05fe6708d7286a
poorvarathore05 <poorvarathore05@gmail.com>
2021-04-20 17:04:44 +0530

    using the session for responses

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,27 +8,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'survey_app'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
-response = []
+RESPONSE_KEY = 'responses'
 
 
 @app.route('/')
 def home_page():
+    session[RESPONSE_KEY] = []
     return render_template('start_survey.html', survey=satisfaction_survey)
 
 
 @app.route('/question/<int:id>')
 def question_page(id):
 
-    if (response is None):
+    responses = session.get(RESPONSE_KEY)
+
+    if (responses is None):
         return redirect('/')
 
-    if (len(response) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(satisfaction_survey.questions)):
         return redirect('/complete')
 
-    if (len(response) != id):
-        flash('Invalid Question Id :' + id)
-        return redirect(f'/question/{len(response)}')
+    if (len(responses) != id):
+        flash(f'Invalid Question Id :{id}')
+        return redirect(f'/question/{len(responses)}')
 
     question = satisfaction_survey.questions[id]
     return render_template('question.html', question=question, ques_num=id)
@@ -38,13 +40,15 @@ def question_page(id):
 def answer_page():
 
     choice = request.form['answer']
-    response.append(choice)
+    responses = session[RESPONSE_KEY]
+    responses.append(choice)
+    session[RESPONSE_KEY] = responses
 
-    if (len(response) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(satisfaction_survey.questions)):
         return redirect('/complete')
 
     else:
-        return redirect(f'question/{len(response)}')
+        return redirect(f'question/{len(responses)}')
 
 
 @app.route('/complete')
